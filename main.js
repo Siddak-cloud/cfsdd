2d65d3d302d1e41ed4170589ed03ff2afdf79577
Siddak-cloud <85190641+Siddak-cloud@users.noreply.github.com>
2021-10-27 20:55:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550 , 500);
+
+    canvas = createCanvas(550 , 450);
+    canvas.position(560 , 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses); 
+}
+
+function draw() {
+    background('#969A97');
+    document.getElementById("hello").innerHTML = "font-size of the text will be = " + difference + "px";
+    fill('#ADD8E6');
+    textSize(difference);
+    text('Siddak' , 50 , 400)
+
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized!');
+}
+
+function gotPoses(results) {
+if(results.length > 0)
+{
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+}
+}
